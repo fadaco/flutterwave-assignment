5cc3e9cf91328514e98d6484f070c287fdf87d2d
fadaco <adedamola.elijah@gmail.com>
2021-01-30 20:40:18 +0100

    first commit

--- router/data.js
+++ router/data.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {JsonResponse} = require('../shared/method');
+const {JsonResponse, checkType, checkCondition, ress} = require('../shared/method');
 const router = express.Router();
 
 const data = {
@@ -25,43 +25,9 @@ const data2 = {
     }
   }
 
-  const vowel = ['a','e','i','o','u'];
+  
 
-  const checkType = (val) => {
-      if(vowel.includes(val.charAt(0).toLowerCase())){
-          return 'an';
-      } else{
-          return 'a'
-      }
-  }
-
-  const checkCondition = (field, val, condition) => {
-      switch(val){
-          case 'gte':
-              return field >= condition;
-          case 'eq':
-              return field == condition;
-          case 'neq':
-              return field != condition;
-          case 'gt':
-              return field > condition;
-          case 'contains':
-              return field.includes(contains);
-          default:
-              return false;
-      }
-  }
-
-  const ress = (status, name, value, condition, condition_value) => {
-   return  {validation : {
-        error : status === 'success' ? false : 'true',
-        field: name,
-        field_value: value,
-        condition: condition,
-        condition_value: condition_value
-      }
-    }
-  }
+ 
   
 
 router.get('/', async (req, res)=> {
